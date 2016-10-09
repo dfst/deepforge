66d7ae1baa7ac2c8aee970adb27ea6bb19f22941
Brian Broll <brian.broll@gmail.com>
2016-10-09 08:31:22 -0500

    WIP #763 basic working example

--- src/visualizers/widgets/PipelineEditor/Connection.js
+++ src/visualizers/widgets/PipelineEditor/Connection.js
@@ -19,13 +19,13 @@ define([
 
     Connection.prototype.setStartPoint = function(point) {
         // Update 'this.points' to start at the given point
-        this.points[0] = point;
+        //this.points[0] = point;
     };
 
     Connection.prototype.setEndPoint = function(point) {
         // Update 'this.points' to end at the given point
-        var last = this.points.length-1;
-        this.points[last] = point;
+        //var last = this.points.length-1;
+        //this.points[last] = point;
     };
 
     return Connection;
