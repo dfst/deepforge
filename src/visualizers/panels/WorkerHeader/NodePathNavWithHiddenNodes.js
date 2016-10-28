076379b4b723b2a6ce76a550fc3274d92b538c31
Brian Broll <brian.broll@gmail.com>
2016-10-28 13:50:53 -0500

    WIP #828 Fixed flashing when clicking 'HOME' on the breadcrumb path

--- src/visualizers/panels/WorkerHeader/NodePathNavWithHiddenNodes.js
+++ src/visualizers/panels/WorkerHeader/NodePathNavWithHiddenNodes.js
@@ -1,4 +1,4 @@
-/* globals define*/
+/* globals define, WebGMEGlobal*/
 define([
     'js/Constants',
     'panels/BreadcrumbHeader/NodePathNavigator'
@@ -32,10 +32,48 @@ define([
                 delete this.territories[nodeIds[i]];
             }
             nodeIds.splice(1, lastRootChildIndex);
-            //nodeIds.unshift(CONSTANTS.PROJECT_ROOT_ID);
         }
 
         return nodeIds;
+    };
+
+    NodePathWithHidden.prototype.addNode = function(id, isActive) {
+        if (id === CONSTANTS.PROJECT_ROOT_ID && !isActive) {
+            var item = document.createElement('li'),
+                anchor = document.createElement('a');
+
+            this._nodes[id] = anchor;
+            item.appendChild(anchor);
+            item.addEventListener('click', () => {
+                var nodeId = this._nodeHistory[1],
+                    node;
+
+                if (nodeId) {
+                    // Get the id for the child of the root node
+                    node = this.client.getNode(nodeId);
+                    if (node.getParentId() !== CONSTANTS.PROJECT_ROOT_ID) {
+                        nodeId = node.getParentId();
+                    }
+                } else {
+                    // Try to load the 'MyPipelines' child of the root node
+                    node = this.client.getNode(CONSTANTS.PROJECT_ROOT_ID)
+                        // Get the child nodes
+                        .getChildrenIds().map(id => this.client.getNode(id))
+                        // Find the child named 'MyPipelines'
+                        .find(child => child && child.getAttribute('name') === 'MyPipelines');
+
+                    if (node) {
+                        nodeId = node.getId();
+                    }
+                }
+                // If none are loaded, try to register MyPipelines
+                WebGMEGlobal.State.registerActiveObject(nodeId || id);
+            });
+            this.territories[id] = {children: 0};
+            this.pathContainer.append(item);
+        } else {
+            return NodePathNavigator.prototype.addNode.apply(this, arguments);
+        }
     };
 
     return NodePathWithHidden;
