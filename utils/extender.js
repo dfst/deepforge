6feec6baff783a730a7009313111082fe261aa53
Brian Broll <brian.broll@gmail.com>
2017-01-09 20:08:24 -0600

    WIP #945 Added ExportFormat:Pipeline installation

--- utils/extender.js
+++ utils/extender.js
@@ -7,6 +7,7 @@
 var path = require('path'),
     fs = require('fs'),
     exists = require('exists-file'),
+    makeTpl = require('lodash.template'),
     CONFIG_DIR = path.join(process.env.HOME, '.deepforge'),
     EXT_CONFIG_NAME = 'extension.json',
     EXTENSION_REGISTRY_NAME = 'extensions.json',
@@ -41,13 +42,40 @@ extender.getInstalledConfig = function(name) {
 
 // Extension Types
 // TODO
-extender.install.ExportFormat = (config, project) => {
-    // TODO: install export format...
-    console.log('installing...');
+extender.install['ExportFormat:Pipeline'] = (config, project) => {
+    var installedExts,
+        PLUGIN_ROOT = path.join(__dirname, '..', 'src', 'plugins', 'GenerateExecFile'),
+        dstPath,
+        content;
+
+    // add the config to the current installed extensions of this type
+    installedExts = allExtConfigs['ExportFormat:Pipeline'] = allExtConfigs['ExportFormat:Pipeline'] || [];
+    installedExts.push(config);
+
+    // copy the main script to src/plugins/GenExecFile/formats/<name>/<main>
+    dstPath = path.join(PLUGIN_ROOT, 'formats', config.name);
+    if (!exists.sync(dstPath)) {
+        fs.mkdirSync(dstPath);
+    }
+
+    try {
+        content = fs.readFileSync(path.join(project.root, config.main), 'utf8');
+    } catch (e) {
+        throw 'Could not read the extension\'s main file: ' + e;
+    }
+    dstPath = path.join(dstPath, path.basename(config.main));
+    fs.writeFileSync(dstPath, content);
+
+    // regenerate the format.js file from the template
+    var formatTemplate = makeTpl(fs.readFileSync(path.join(PLUGIN_ROOT, 'format.js.ejs'), 'utf8')),
+        formatsIndex = formatTemplate({formats: installedExts});
+
+    dstPath = path.join(PLUGIN_ROOT, 'format.js');
+    fs.writeFileSync(dstPath, formatsIndex);
     persistExtConfig();
 };
 
-extender.uninstall.ExportFormat = name => {
+extender.uninstall['ExportFormat:Pipeline'] = name => {
 };
 
 module.exports = extender;
