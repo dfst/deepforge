ce7c042bc7f694de7b7eedfae46881917cf505fa
Brian Broll <brian.broll@gmail.com>
2017-11-30 18:35:34 -0600

    WIP #497 removed old commented out code

--- test/features/operations/OperationEditor.spec.js
+++ test/features/operations/OperationEditor.spec.js
@@ -270,9 +270,6 @@ describe('Operations', function() {
                 }, 1500, 'Expected output node to be removed within 1.5s');
             });
 
-            // remove reference (need a new test op)
-            // TODO
-
             it('should update model on rename', function() {
                 const newName = 'TestRename';
                 browser.url(existingOperationUrl);
@@ -282,43 +279,10 @@ describe('Operations', function() {
                     return title === newName;
                 }, 15000, 'Expected output node to be removed within 1.5s');
             });
+
+            // remove reference (need a new test op)
+            // TODO
+
         });
     });
-
-    describe.skip('basic operations', function() {
-        //beforeEach(function() {  // open the project
-            //let projectBtn = '.project-list .open-link';
-            //browser.url(URL);
-            //browser.waitForVisible(projectBtn, 10000);
-            //browser.click(projectBtn);
-        //});
-
-        it('should create project', function(done) {
-            browser.url(URL);
-            browser.waitForVisible('.btn-create-new', 10000);
-            setTimeout(done, 5000);
-            //browser.click('.btn-create-new');
-            //browser.setValue('.txt-project-name', PROJECT_NAME);
-            //browser.click('.btn-save');
-            //browser.waitForVisible('.btn-create-snap-shot', 10000);
-            //browser.click('.btn-create-snap-shot');
-            //browser.waitForVisible('.background-text', 10000);
-        });
-    });
-
-    //describe('visual-textual sync', function() {
-        //before(function() {
-            //// Create the operation
-            //browser.click(S.ACTION_BTN);
-            //browser.click(S.ACTION_BTN);
-            //browser.waitForEnabled(S.NEW_OPERATION, 10000);
-            //browser.click(S.NEW_OPERATION);
-            //browser.waitForEnabled(S.INT.OPERATION, 10000);
-        //});
-
-        //it('should add textual input on adding visual input', function() {
-            //browser.click(S.INT.OPERATION);
-            //// TODO: add textual input
-        //});
-    //});
 });
