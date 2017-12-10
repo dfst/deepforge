0d79ff862373f05792d241c9b05696f11c6c8c91
Brian Broll <brian.broll@gmail.com>
2017-12-09 21:01:41 -0600

    WIP #1043 started import library fn-ality

--- src/plugins/ImportLibrary/ImportLibrary.js
+++ src/plugins/ImportLibrary/ImportLibrary.js
@@ -3,9 +3,11 @@
 
 define([
     'plugin/UploadSeedToBlob/UploadSeedToBlob/UploadSeedToBlob',
+    'webgme/src/bin/import',
     'text!./metadata.json'
 ], function (
     PluginBase,
+    ImportProject,
     pluginMetadata
 ) {
 
@@ -48,8 +50,11 @@ define([
         const config = this.getCurrentConfig();
         const libraryInfo = config.libraryInfo;
 
-        return this.uploadSeed(libraryInfo.seed)
-            .then(hash => this.core.addLibrary(this.rootNode, library.name, hash))
+        return this.addSeedToBranch(libraryInfo.seed)
+            .then(branchName => this.createGMELibraryFromBranch(branchName, libraryInfo))
+            .then(branch => this.removeTemporaryBranch(branch))
+            .then(() => this.updateMetaForLibrary(libraryInfo))
+            .then(() => this.save(`Imported ${libraryInfo.name} library`))
             .then(() => {
                 this.result.setSuccess(true);
                 callback(null, this.result);
@@ -58,13 +63,79 @@ define([
                 this.logger.error(`Could not check the libraries: ${err}`);
                 callback(err, this.result);
             });
-
-        // update the meta with the types
-        // TODO
     };
 
-    //ImportLibrary.prototype.main = function (callback) {
-    //};
+    ImportLibrary.prototype.getUniqueBranchName = function (basename) {
+        const branches = this.project.branches;
+        let name = basename;
+        let i = 2;
+
+        while (branches[name]) {
+            name = `${basename} ${i}`;
+            i++;
+        }
+        return name;
+    };
+
+    ImportLibrary.prototype.addSeedToBranch = function (name) {
+        const filepath = this.getSeedDataPath(name);
+        const project = this.projectName;
+        const branch = this.getUniqueBranchName(`importLibTmpBranch${name}`);
+        const argv = `node import ${filepath} -p ${project} -b ${branch}`.split(' ');
+
+        return this.project.createBranch(name, this.commitHash)
+            .then(() => ImportProject.main(argv))
+            .then(() => branch);
+    };
+
+    ImportLibrary.prototype.createGMELibraryFromBranch = function (branchName, libraryInfo) {
+        const name = libraryInfo.name;
+        const libraryData = {
+            projectId: this.projectId,
+            branchName: branchName,
+            commitHash: null
+        };
+
+        console.log('main hash', this.commitHash);
+        // TODO: check the branch hash?
+        return this.project.getBranchHash(branchName)
+            .then(hash => {
+                // TODO: get root hash?
+                console.log('library hash', hash);
+                libraryData.commitHash = hash;
+                return this.core.addLibrary(this.rootNode, name, this.commitHash, libraryData);
+            });
+    };
+
+    ImportLibrary.prototype.removeTemporaryBranch = function (branchName) {
+        return this.project.getBranchHash(branchName)
+            .then(hash => this.project.deleteBranch(branchName, hash));
+    };
+
+    const values = obj => Object.keys(obj).map(k => obj[k]);
+    ImportLibrary.prototype.updateMetaForLibrary = function (branchInfo, libraryInfo) {
+        const nodeNames = libraryInfo.nodeTypes;
+        const libraryNodes = values(this.core.getLibraryMetaNodes(this.rootNode, libraryInfo.name));
+
+        // Get each node from 'nodeTypes'
+        const nodes = nodeNames
+            .map(name => {
+                const node = libraryNodes.find(node => {
+                    return this.core.getAttribute(node, 'name') === name;
+                });
+                if (!node) this.logger.warn(`Could not find ${name} in ${libraryInfo.name}. Skipping...`);
+                return node;
+            })
+            .filter(node => !!node);
+
+        // Add containment relationships to the meta
+        return this.core.loadChildren(this.rootNode)
+            .then(children => {
+                let parent = children.find(node => this.core.getAttribute(node, 'name') === 'MyResources');
+                if (!parent) throw new Error('Could not find resources location');
+                nodes.forEach(node => this.core.setChildMeta(parent, node));
+            });
+    };
 
     return ImportLibrary;
 });
