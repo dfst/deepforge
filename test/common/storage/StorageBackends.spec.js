4193748f61dfb781f6b0988df9adea9d3ab62215
Umesh <umesh.timalsina@vanderbilt.edu>
2020-01-24 13:29:26 -0600

    #1399: Add Storage Tests

---
+++ test/common/storage/StorageBackends.spec.js
@@ -0,0 +1,61 @@
+/* eslint-env node, mocha */
+describe('Storage Features Test', function () {
+    const testFixture = require('../../globals');
+    const {promisify} = require('util');
+    const {requirejs} = testFixture;
+    const TEST_STORAGE = 'storageFeaturesSpec';
+    const logger = testFixture.logger.fork('StorageTests');
+    const Storage = requirejs('deepforge/storage/index');
+    const gmeConfig = testFixture.getGmeConfig();
+    const server = new testFixture.WebGME.standaloneServer(gmeConfig);
+    server.start = promisify(server.start);
+    server.stop = promisify(server.stop);
+
+
+    const storageBackends = Storage.getAvailableBackends();
+    let StorageConfigs,
+        client,
+        clients = {},
+        dataInfo;
+
+    before(async function () {
+        this.timeout(5000);
+        await server.start();
+        StorageConfigs = await testFixture.getStorageConfigs();
+        for (const backend of storageBackends) {
+            client = await Storage.getClient(backend, logger, StorageConfigs[backend]);
+            clients[backend] = client;
+            const nop = () => {
+            };
+            await client.deleteDir(TEST_STORAGE).catch(nop);
+        }
+    });
+
+    for (const backend of storageBackends) {
+        it(`Should execute putFile operation for the storage backend ${backend} `, async () => {
+            this.timeout(5000);
+            dataInfo = await clients[backend].putFile(`${TEST_STORAGE}/dummyFile`,
+                Buffer.from('A Quick Brown Fox Jumped over a lazy Dog.'));
+        });
+
+        it(`Should execute getFile operation for the storage backend ${backend}`, async () => {
+            this.timeout(5000);
+            await clients[backend].getFile(dataInfo);
+        });
+
+        it(`Should execute getCachePath operation for the storage backend ${backend}`, async () => {
+            this.timeout(5000);
+            await clients[backend].getCachePath(dataInfo);
+        });
+
+        it(`Should execute deleteFile Operation for the storage backend ${backend}`, async () => {
+            this.timeout(5000);
+            await clients[backend].deleteFile(dataInfo);
+        });
+    }
+
+
+    after(async function () {
+        await server.stop();
+    });
+});
