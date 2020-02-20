b05d3ba8e5eaadff2fcaaf4656556bd4922e05de
Umesh <umesh.timalsina@vanderbilt.edu>
2020-02-20 15:27:33 -0600

    WIP-#1386: Complete Implementation of s3 Storage using aws-sdk

--- src/common/storage/backends/s3/Client.js
+++ src/common/storage/backends/s3/Client.js
@@ -6,8 +6,8 @@ define([
 ) {
     const S3Storage = function (id, name, logger, config = {}) {
         StorageClient.apply(this, arguments);
-        this.bucketName = config.bucketName;
-        this.endpoint = config.endpoint;
+        this.bucketName = config.bucketName || 'deepforge';
+        this.endpoint = config.endpoint || 'http://localhost:80';
         this.config = this.createS3Config(config);
         this.s3Client = null;
         this.ready = this.initialize();
@@ -15,13 +15,37 @@ define([
 
     S3Storage.prototype = Object.create(StorageClient.prototype);
 
+
     S3Storage.prototype.initialize = async function () {
-        const AWS = require.isBrowser ? await require('aws-sdk-min') :
-            require.nodeRequire('aws-sdk');
+        if (require.isBrowser) {
+            return new Promise((resolve, reject) => {
+                try {
+                    require(['aws-sdk-min'], () => {
+                        /* eslint-disable no-undef*/
+                        this.initializeS3Client(AWS);
+                        /* eslint-disable no-undef*/
+                        resolve();
+                    });
+                } catch (err) {
+                    reject(err);
+                }
+            });
+        } else {
+            const AWS = require.nodeRequire('aws-sdk');
+            this.initializeS3Client(AWS);
+        }
+    };
+
+    S3Storage.prototype.initializeS3Client = function (AWS) {
         this.s3Client = new AWS.S3(this.config);
-        promisifyMethod(this.s3Client, 'createBucket');
-        promisifyMethod(this.s3Client, 'getObject');
-        promisifyMethod(this.s3Client, 'putObject');
+        if (this.s3Client) {
+            promisifyMethod(this.s3Client, 'createBucket');
+            promisifyMethod(this.s3Client, 'getObject');
+            promisifyMethod(this.s3Client, 'putObject');
+            promisifyMethod(this.s3Client, 'deleteObject');
+            promisifyMethod(this.s3Client, 'headObject');
+            promisifyMethod(this.s3Client, 'listObjectsV2');
+        }
     };
 
     S3Storage.prototype.getS3Client = async function () {
@@ -31,7 +55,7 @@ define([
 
     S3Storage.prototype.createS3Config = function (config) {
         return {
-            endpoint: config.endpoint || 'http://localhost:80',
+            endpoint: config.endpoint || 'http://localhost:9000',
             accessKeyId: config.accessKeyId,
             secretAccessKey: config.secretAccessKey,
             sslEnabled: config.endpoint ? config.endpoint.startsWith('https') : false,
@@ -41,44 +65,49 @@ define([
     };
 
     S3Storage.prototype.createBucketIfNeeded = async function () {
-        return this.s3Client.createBucket({
-            Bucket: this.bucketName
-        });
+        const s3Client = await this.getS3Client();
+        try {
+            await s3Client.createBucket({
+                Bucket: this.bucketName
+            });
+        } catch (err) {
+            if (err['statusCode'] !== 409) {
+                this.logger.error(`Failed to create bucket ${this.bucketName} in S3 server.`);
+                throw err;
+            }
+
+        }
     };
 
     S3Storage.prototype.getFile = async function (dataInfo) {
+        await this.stageConfigAndCreateNewS3Client(dataInfo);
         const {bucketName, filename} = dataInfo.data;
-        const params = {
+        const s3Client = await this.getS3Client();
+        const data = await s3Client.getObject({
             Bucket: bucketName,
             Key: filename
-        };
-        const data = await this.s3Client.getObject(params);
+        });
+        await this.restoreS3Client();
         return data.Body;
     };
 
     S3Storage.prototype.putFile = async function (filename, content) {
-        try {
-            await this.createBucketIfNeeded();
-            this.logger.debug(`Created bucket ${this.bucketName}`);
-        } catch (err) {
-            if (err.statusCode !== 409) {
-                this.logger.error(`Failed to create bucket ${this.bucketName} with error ${JSON.stringify(err)}`);
-                throw err;
-            }
-        }
+        await this.createBucketIfNeeded();
+        this.logger.debug(`Created bucket ${this.bucketName}`);
         const params = {
             Body: require.isBrowser ? new Blob([content]) : content,
             Bucket: this.bucketName,
             Key: filename,
         };
-        await this.s3Client.putObject(params);
+        const s3Client = await this.getS3Client();
+        await s3Client.putObject(params);
         const metadata = await this._stat(filename, this.bucketName);
         metadata.filename = filename;
         metadata.size = metadata.ContentLength;
         metadata.bucketName = this.bucketName;
         metadata.endpoint = this.config.endpoint;
+        this.logger.debug(`Successfully uploaded file ${filename} to the S3 server.`);
         return this.createDataInfo(metadata);
-        //this.logger.error(`File upload to s3 Storage unsuccessful with error ${JSON.stringify(err)}`);
     };
 
     S3Storage.prototype._stat = async function (path, bucketName) {
@@ -86,61 +115,58 @@ define([
             Bucket: bucketName,
             Key: path
         };
-        return new Promise((resolve, reject) => this.s3Client.headObject(params, (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        }));
+        return (await this.getS3Client()).headObject(params);
     };
 
     S3Storage.prototype.deleteDir = async function (dirname) {
         let dataInfo;
-        return new Promise((resolve, reject) => {
-            this.s3Client.listObjectsV2({
-                Bucket: this.bucketName,
-                MaxKeys: 1000,
-                Prefix: dirname
-            }, async (err, data) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    try {
-                        for (const file of data.Contents) {
-                            dataInfo = {
-                                bucketName: this.bucketName,
-                                Key: file.Key
-                            };
-                            await this.deleteFile(dataInfo);
-                        }
-                    } catch (err) {
-                        reject(err);
-                    }
-                    resolve(`Successfully deleted directory/path ${dirname} in the S3 server`);
-                }
-            });
+        const s3Client = await this.getS3Client();
+        const {Contents} = await s3Client.listObjectsV2({
+            Bucket: this.bucketName,
+            MaxKeys: 1000,
+            Prefix: dirname
         });
+
+        for (const file of Contents) {
+            dataInfo = {
+                data: {
+                    bucketName: this.bucketName,
+                    filename: file.Key,
+                    endpoint: this.endpoint
+                }
+            };
+            await this.deleteFile(dataInfo);
+        }
+        this.logger.debug(`Successfully deleted directory ${dirname} from the S3 server`);
     };
 
     S3Storage.prototype.deleteFile = async function (dataInfo) {
+        await this.stageConfigAndCreateNewS3Client(dataInfo);
         const {bucketName, filename} = dataInfo.data;
         const params = {
             Bucket: bucketName,
             Key: filename
         };
-        return new Promise((resolve, reject) => {
-            this.s3Client.deleteObject(params, (err, data) => {
-                if (err) {
-                    this.logger.error(`Failed to delete object from s3 server with error ${err}`);
-                    reject(err);
-                } else {
-                    resolve(data);
-                }
-            });
-        });
-
+        await this.s3Client.deleteObject(params);
+        await this.restoreS3Client();
     };
+
+    S3Storage.prototype.stageConfigAndCreateNewS3Client = async function(dataInfo) {
+        this.stagedConfig = JSON.parse(JSON.stringify(this.config));
+        const {endpoint} = dataInfo.data;
+        const {accessKeyId, secretAccessKey} = this.config;
+        this.config = this.createS3Config({endpoint, accessKeyId, secretAccessKey});
+        this.ready = await this.initialize();
+    };
+
+    S3Storage.prototype.restoreS3Client = async function () {
+        if (this.stagedConfig) {
+            this.config = this.stagedConfig;
+            this.ready = await this.initialize();
+            this.stagedConfig = null;
+        }
+    };
+
 
     S3Storage.prototype.getMetadata = async function (dataInfo) {
         const metadata = {size: dataInfo.data.size};
@@ -154,10 +180,10 @@ define([
 
     function promisifyMethod(object, method) {
         const fn = object[method];
-        object[method] = function() {
+        object[method] = function () {
             return new Promise((resolve, reject) => {
                 const args = Array.prototype.slice.call(arguments);
-                const callback = function(err, result) {
+                const callback = function (err, result) {
                     if (err) {
                         return reject(err);
                     }
