c047cc47d860d0a5eeafd24f3af9137e02f5b554
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2020-04-01 09:21:19 -0500

    Remove tensorflow fix from worker environment in dev entrypoint

--- .deployment/dev-entrypoint.sh
+++ .deployment/dev-entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# Remove pypi tensorflow from deepforge
-source activate deepforge
-pip uninstall tensorflow -y
-
-# Install tensorlfow from conda
-conda install tensorflow==1.14 -y
-
 # Remove pypi tensorflow from deepforge-server
 source activate deepforge-server
 pip uninstall tensorflow -y
