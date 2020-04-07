78cc0a35c239bc3d355ec14bb564c793a0c0409b
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2020-04-07 18:58:43 -0500

    Remove local compute from deployment. Closes #1605

--- .deployment/dev-entrypoint.sh
+++ .deployment/dev-entrypoint.sh
@@ -4,4 +4,9 @@ source activate deepforge-server
 pip uninstall tensorflow -y
 conda install tensorflow==1.14 -y
 
+# Install jq and remove local from config/components.json file
+sudo apt-get install jq --yes
+< config/components.json  jq '.Compute.backends=(.Compute.backends | map(select(. != "local")))' \
+> config/components2.json && mv config/components2.json config/components.json
+
 deepforge start --server
