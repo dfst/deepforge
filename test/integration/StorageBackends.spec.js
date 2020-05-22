06a715cda013eba3b16225340947b5e804de5fe0
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2020-05-21 21:26:12 -0500

    Refactor StorageBackends.spec.js to use a single function. (Closes #1718)

--- test/integration/StorageBackends.spec.js
+++ test/integration/StorageBackends.spec.js
@@ -1,12 +1,13 @@
 /* eslint-env node, mocha */
 describe('Storage Features Test', function () {
-    this.timeout(5000);
+    this.timeout(10000);
     const assert = require('assert');
     const testFixture = require('../globals');
     const {promisify} = require('util');
     const {requirejs} = testFixture;
     const TEST_STORAGE = 'storageFeaturesSpec';
     const TEST_PATH = `${TEST_STORAGE}/dummyFile`;
+    const TEST_BUFFER = Buffer.from('A Quick Brown Fox Jumped over a lazy Dog.');
     const logger = testFixture.logger.fork('StorageTests');
     const Storage = requirejs('deepforge/storage/index');
     const gmeConfig = testFixture.getGmeConfig();
@@ -18,8 +19,7 @@ describe('Storage Features Test', function () {
     const storageBackends = Storage.getAvailableBackends();
     let StorageConfigs,
         client,
-        clients = {},
-        dataInfo;
+        clients = {};
 
     before(async function () {
         await server.start();
@@ -33,36 +33,31 @@ describe('Storage Features Test', function () {
     });
 
     for (const backend of storageBackends) {
-        it(`should putFile using ${backend}`, async function() {
+        it(`should run storageLoop using ${backend}`, async function() {
             this.retries(maxRetries(backend));
-            dataInfo = await clients[backend].putFile(TEST_PATH,
-                Buffer.from('A Quick Brown Fox Jumped over a lazy Dog.'));
+            await storageLoop(clients[backend], TEST_PATH, TEST_BUFFER);
         });
+    }
 
-        it(`should getFile using ${backend}`, async function() {
-            this.retries(maxRetries(backend));
-            await clients[backend].getFile(dataInfo);
-        });
+    async function storageLoop(client, testPath, content) {
+        const dataInfo = await client.putFile(testPath, content);
+        const contentCopy = await client.getFile(dataInfo);
+        assert(content.toString() === contentCopy.toString());
 
-        it(`should getCachePath using ${backend}`, async () => {
-            await clients[backend].getCachePath(dataInfo);
-        });
+        await client.getCachePath(dataInfo);
 
-        it(`should stat file using ${backend}`, async () => {
-           if(backend !== 'gme'){
-               await clients[backend].stat(TEST_PATH);
-           } else {
-               assert.rejects(clients[backend].stat(TEST_PATH), {
-                   name: 'Error',
-                   message: 'stat not implemented for WebGME Blob Storage'
-               });
-           }
-        });
+        if(client.id !== 'gme') {
+            const dataInfoCopy = await client.stat(testPath);
+            assert(dataInfo.data.filename === dataInfoCopy.data.filename);
+            assert(dataInfo.data.size === dataInfoCopy.data.size);
+        } else {
+            assert.rejects(client.stat(testPath), {
+                name: 'Error',
+                message: 'stat not implemented for WebGME Blob Storage'
+            });
+        }
 
-        it(`should deleteFile using ${backend}`, async function() {
-            this.retries(maxRetries(backend));
-            await clients[backend].deleteFile(dataInfo);
-        });
+        await client.deleteFile(dataInfo);
     }
 
     after(async function () {
