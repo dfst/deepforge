2aa6a4f2e81815f474189bc9ed6bf95c68011318
Brian Broll <brian.broll@gmail.com>
2020-06-24 14:27:05 -0500

    Remove debug logs

--- test/integration/ExecutePipeline.spec.js
+++ test/integration/ExecutePipeline.spec.js
@@ -39,12 +39,9 @@ describe('Pipeline execution', function () {
 
     before(async function () {
         this.timeout(20000);
-        console.log('getting gmeAuth');
         gmeAuth = await testFixture.clearDBAndGetGMEAuth(gmeConfig, projectName);
         // This uses in memory storage. Use testFixture.getMongoStorage to persist test to database.
-        console.log('getting storage');
         storage = testFixture.getMemoryStorage(logger, gmeConfig, gmeAuth);
-        console.log('opening db');
         await storage.openDatabase();
         const importParam = {
             projectSeed: path.join(testFixture.DF_SEED_DIR, 'devProject', 'devProject.webgmex'),
@@ -54,16 +51,12 @@ describe('Pipeline execution', function () {
             gmeConfig: gmeConfig
         };
 
-        console.log('importing...');
         const result = await testFixture.importProject(storage, importParam);
         project = result.project;
         commitHash = result.commitHash;
 
-        console.log('creating branch');
         await project.createBranch('test', commitHash);
-        console.log('starting server');
         await server.start();
-        console.log('starting worker');
         worker = await startWorker();
     });
 
@@ -185,7 +178,6 @@ describe('Pipeline execution', function () {
             let stdout = '';
             subprocess.stdout.on('data', data => {
                 if (stdout !== null) {
-                    console.log('--> ', data.toString());
                     stdout += data.toString();
                     if (stdout.includes('Connected')) {
                         stdout = null;
