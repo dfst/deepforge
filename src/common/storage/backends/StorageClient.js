78774646bf11a279bd4223a1083e243f135b4b52
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2020-07-16 14:39:53 -0500

    Refactor StorageHelpers.js back to StorageClient.js

--- src/common/storage/backends/StorageClient.js
+++ src/common/storage/backends/StorageClient.js
@@ -2,12 +2,13 @@
 define([
     'client/logger',
     'deepforge/gmeConfig',
-    '../StorageHelpers'
 ], function(
     Logger,
-    gmeConfig,
-    StorageHelpers
+    gmeConfig
 ) {
+    const fetch = require.isBrowser ? window.fetch :
+        require.nodeRequire('node-fetch');
+    const Headers = require.isBrowser ? window.Headers : fetch.Headers;
     const StorageClient = function(id, name, logger) {
         this.id = id;
         this.name = name;
@@ -15,6 +16,30 @@ define([
             logger = Logger.create(`gme:storage:${id}`, gmeConfig.client.log);
         }
         this.logger = logger.fork(`storage:${id}`);
+    };
+
+    const StorageHelpers = {};
+
+    StorageHelpers.getServerURL = function () {
+        const {port} = gmeConfig.server;
+        let url = require.isBrowser ? window.origin :
+            (process.env.DEEPFORGE_HOST || `http://127.0.0.1:${port}`);
+        return [url.replace(/^https?:\/\//, ''), url.startsWith('https')];
+    };
+
+    StorageHelpers.getURL = function (url) {
+        return url;
+    };
+
+    StorageHelpers.fetch = async function (url, opts = {}) {
+        url = this.getURL(url);
+        opts.headers = new Headers(opts.headers || {});
+        const response = await fetch(url, opts);
+        const {status} = response;
+        if (status > 399) {
+            return Promise.reject(response);
+        }
+        return response;
     };
 
     Object.assign(StorageClient.prototype, StorageHelpers);
