14f83d63d9f06581f14eac53badf6b9870b9df28
Brian Broll <brian.broll@gmail.com>
2020-08-04 16:42:43 -0500

    Add auth to config for gme blob. Fixes #1645

---
+++ src/common/storage/backends/gme/metadata.js
@@ -0,0 +1,24 @@
+/*global define*/
+define([
+    'deepforge/gmeConfig',
+], function(
+    config,
+) {
+    const metadata = {
+        name: 'WebGME Blob Storage',
+        configStructure: []
+    };
+
+
+    if (config.authentication.enable) {
+        metadata.configStructure.push({
+            name: 'apiToken',
+            displayName: 'Access Token',
+            value: '',
+            valueType: 'string',
+            readOnly: false,
+            isAuth: true,
+        });
+    }
+    return metadata;
+});
