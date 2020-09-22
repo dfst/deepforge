6e0ca24f734c31f3da9d8bc149cb1363745939df
Brian Broll <brian.broll@gmail.com>
2020-09-22 15:32:20 -0500

    Use deepforge serializer for models

--- src/visualizers/widgets/TrainKeras/Main.py
+++ src/visualizers/widgets/TrainKeras/Main.py
@@ -1,6 +1,9 @@
 import utils.init
 from operations.train import Train
 from artifacts.<%= dataset.name %> import data
+import deepforge
+import os
+import json
 
 <%= archCode %>
 model = result
@@ -8,5 +11,11 @@ model = result
 train = Train(model)
 model = train.execute(data)
 
-# TODO: Save this as an output/artifact
-model.save('<%= path %>')
+os.makedirs('outputs/<%= path %>/', exist_ok=True)
+with open('outputs/<%= path %>/metadata.json', 'w') as outfile:
+    metadata = {}
+    metadata['type'] = deepforge.serialization.get_full_class_name(model)
+    json.dump(metadata, outfile)
+
+with open('outputs/<%= path %>/data', 'wb') as outfile:
+    deepforge.serialization.dump(model, outfile)
