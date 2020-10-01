8360fd4a1e6f4cf119f4fbfdc2e0fa28f0b2bb12
Brian Broll <brian.broll@gmail.com>
2020-10-01 14:09:49 -0500

    Add dependencies to interactive editors

--- src/visualizers/panels/InteractiveWorkspace/InteractiveWorkspaceControl.js
+++ src/visualizers/panels/InteractiveWorkspace/InteractiveWorkspaceControl.js
@@ -17,127 +17,138 @@ define([
     'use strict';
 
     InteractiveEditors = JSON.parse(InteractiveEditors);
-    function InteractiveWorkspaceControl(options) {
+    class InteractiveWorkspaceControl{
+        constructor(options) {
 
-        this._logger = options.logger.fork('Control');
+            this._logger = options.logger.fork('Control');
 
-        this._client = options.client;
+            this._client = options.client;
 
-        // Initialize core collections and variables
-        this._widget = options.widget;
+            // Initialize core collections and variables
+            this._widget = options.widget;
 
-        this._currentNodeId = null;
-        this._currentNodeParentId = undefined;
+            this._currentNodeId = null;
+            this._currentNodeParentId = undefined;
 
-        this.editors = [];
-        this._initWidgetEventHandlers();
-        this._logger.debug('ctor finished');
-    }
-
-    InteractiveWorkspaceControl.prototype._initWidgetEventHandlers = function () {
-        InteractiveEditors.forEach(info => {
-            DeepForge.registerAction(
-                `Open ${info.title}`,
-                'add',
-                10,
-                () => this.openEditorBrowser(info)
-            );
-        });
-    };
-
-    InteractiveWorkspaceControl.prototype.openEditorBrowser = async function (editorInfo) {
-        const EditorPanel = await this.require(editorInfo.panel);
-        if (!this.session) {
-            const connectedEditor = this.editors
-                .find(editor => editor.control.session);
-            this.session = connectedEditor && connectedEditor.control.session;
+            this.editors = [];
+            this._initWidgetEventHandlers();
+            this._logger.debug('ctor finished');
         }
 
-        const editor = new EditorPanel(null, {
-            client: this._client,
-            embedded: this._embedded,
-            session: this.session && this.session.fork(),
-        });
+        _initWidgetEventHandlers() {
+            this.getAvailableEditors().forEach(info => {
+                DeepForge.registerAction(
+                    `Open ${info.title}`,
+                    'add',
+                    10,
+                    () => this.openEditor(info)
+                );
+            });
+        }
 
-        editor.control.on('computeInitialized', session => {
-            this.session = session;
-            this.editors.forEach(editor => {
-                const hasSession = !!editor.control.session;
-                if (!hasSession) {
-                    editor.control.onComputeInitialized(session.fork());
+        getAvailableEditors() {
+            const libraries = this._client.getLibraryNames();
+            return InteractiveEditors.filter(editorInfo => {
+                const deps = editorInfo.dependencies || [];
+                const isSupported = deps.every(dep => libraries.includes(dep));
+                return isSupported;
+            });
+        }
+
+        async openEditor(editorInfo) {
+            const EditorPanel = await this.require(editorInfo.panel);
+            if (!this.session) {
+                const connectedEditor = this.editors
+                    .find(editor => editor.control.session);
+                this.session = connectedEditor && connectedEditor.control.session;
+            }
+
+            const editor = new EditorPanel(null, {
+                client: this._client,
+                embedded: this._embedded,
+                session: this.session && this.session.fork(),
+            });
+
+            editor.control.on('computeInitialized', session => {
+                this.session = session;
+                this.editors.forEach(editor => {
+                    const hasSession = !!editor.control.session;
+                    if (!hasSession) {
+                        editor.control.onComputeInitialized(session.fork());
+                    }
+                });
+            });
+            editor.control.on('destroy', () => {
+                const index = this.editors.indexOf(editor);
+                if (index > -1) {
+                    this.editors.splice(index, 1);
                 }
             });
-        });
-        editor.control.on('destroy', () => {
-            const index = this.editors.indexOf(editor);
-            if (index > -1) {
-                this.editors.splice(index, 1);
+
+            if (editor.control && editor.control.selectedObjectChanged) {
+                const nodeId = await DeepForge.places.MyArtifacts();
+                editor.control.selectedObjectChanged(nodeId);
             }
-        });
+            this.editors.push(editor);
 
-        if (editor.control && editor.control.selectedObjectChanged) {
-            const nodeId = await DeepForge.places.MyArtifacts();
-            editor.control.selectedObjectChanged(nodeId);
+            this._widget.addEditor(editorInfo.title, editor);
+            // TODO: Show the modal
+            // TODO: If one selected, import it
         }
-        this.editors.push(editor);
 
-        this._widget.addEditor(editorInfo.title, editor);
-        // TODO: Show the modal
-        // TODO: If one selected, import it
-    };
-
-    InteractiveWorkspaceControl.prototype.require = function (path) {
-        return new Promise((resolve, reject) => require([path], resolve, reject));
-    };
-
-    /* * * * * * * * Visualizer content update callbacks * * * * * * * */
-    InteractiveWorkspaceControl.prototype.selectedObjectChanged = function (nodeId) {
-        this._currentNodeId = nodeId;
-    };
-
-    InteractiveWorkspaceControl.prototype._stateActiveObjectChanged = function (model, activeObjectId) {
-        if (this._currentNodeId === activeObjectId) {
-            // The same node selected as before - do not trigger
-        } else {
-            this.selectedObjectChanged(activeObjectId);
+        require(path) {
+            return new Promise((resolve, reject) => require([path], resolve, reject));
         }
-    };
 
-    /* * * * * * * * Visualizer life cycle callbacks * * * * * * * */
-    InteractiveWorkspaceControl.prototype.destroy = function () {
-        InteractiveEditors.forEach(info => {
-            DeepForge.unregisterAction(`Open ${info.title}`);
-        });
-        this._detachClientEventListeners();
-        if (this.session) {
-            this.session.close();
+        /* * * * * * * * Visualizer content update callbacks * * * * * * * */
+        selectedObjectChanged(nodeId) {
+            this._currentNodeId = nodeId;
         }
-    };
 
-    InteractiveWorkspaceControl.prototype._attachClientEventListeners = function () {
-        this._detachClientEventListeners();
-        WebGMEGlobal.State.on('change:' + CONSTANTS.STATE_ACTIVE_OBJECT, this._stateActiveObjectChanged, this);
-    };
-
-    InteractiveWorkspaceControl.prototype._detachClientEventListeners = function () {
-        WebGMEGlobal.State.off('change:' + CONSTANTS.STATE_ACTIVE_OBJECT, this._stateActiveObjectChanged);
-    };
-
-    InteractiveWorkspaceControl.prototype.onActivate = function () {
-        if (!this._embedded) {
-            this._attachClientEventListeners();
-
-            if (typeof this._currentNodeId === 'string') {
-                WebGMEGlobal.State.registerActiveObject(this._currentNodeId, {suppressVisualizerFromNode: true});
+        _stateActiveObjectChanged(model, activeObjectId) {
+            if (this._currentNodeId === activeObjectId) {
+                // The same node selected as before - do not trigger
+            } else {
+                this.selectedObjectChanged(activeObjectId);
             }
         }
-    };
 
-    InteractiveWorkspaceControl.prototype.onDeactivate = function () {
-        if (!this._embedded) {
+        /* * * * * * * * Visualizer life cycle callbacks * * * * * * * */
+        destroy() {
+            this.getAvailableEditors().forEach(info => {
+                DeepForge.unregisterAction(`Open ${info.title}`);
+            });
             this._detachClientEventListeners();
+            if (this.session) {
+                this.session.close();
+            }
         }
-    };
+
+        _attachClientEventListeners() {
+            this._detachClientEventListeners();
+            WebGMEGlobal.State.on('change:' + CONSTANTS.STATE_ACTIVE_OBJECT, this._stateActiveObjectChanged, this);
+        }
+
+        _detachClientEventListeners() {
+            WebGMEGlobal.State.off('change:' + CONSTANTS.STATE_ACTIVE_OBJECT, this._stateActiveObjectChanged);
+        }
+
+        onActivate() {
+            if (!this._embedded) {
+                this._attachClientEventListeners();
+
+                if (typeof this._currentNodeId === 'string') {
+                    WebGMEGlobal.State.registerActiveObject(this._currentNodeId, {suppressVisualizerFromNode: true});
+                }
+            }
+        }
+
+        onDeactivate() {
+            if (!this._embedded) {
+                this._detachClientEventListeners();
+            }
+        }
+    }
 
     return InteractiveWorkspaceControl;
 });
